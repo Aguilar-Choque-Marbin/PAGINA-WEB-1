db6f98def5a96362ef92424d985715784dc72507
jheferson69 <alfaomegajhismar@gmail.com>
2024-05-21 07:16:14 -0400

    1

--- Script1.js
+++ Script1.js
@@ -1,1 +1,13 @@
 // JavaScript source code
+// Esperar a que el documento esté completamente cargado
+document.addEventListener("DOMContentLoaded", function () {
+    // Seleccionar el botón y el elemento de mensaje por sus IDs
+    var boton = document.getElementById("cambiarMensaje");
+    var mensaje = document.getElementById("mensaje");
+
+    // Añadir un evento de clic al botón
+    boton.addEventListener("click", function () {
+        // Cambiar el contenido del elemento de mensaje
+        mensaje.textContent = "¡Este mensaje ha sido cambiado por script.js!";
+    });
+});
